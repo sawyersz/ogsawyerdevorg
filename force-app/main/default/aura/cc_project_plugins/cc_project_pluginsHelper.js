a7cddcdafde74972660fa9f931a72d08e2cb07ce
Sawyer Kennell <38391716+sawyersz@users.noreply.github.com>
2019-12-11 14:46:19 -0700

    created plugins file/ exploring accordion

---
+++ force-app/main/default/aura/cc_project_plugins/cc_project_pluginsHelper.js
@@ -0,0 +1,18 @@
+({
+    tabs : {
+        summary : [
+            {
+                label : 'Hello World 00000',
+                component : 'c:helloworld'
+            }
+        ],
+        timelines : [],
+        documents : [],
+        controls : [],
+        resources : [],
+        financials : []
+    },
+    getTabs : function(){
+        return this.tabs; 
+    },
+})
