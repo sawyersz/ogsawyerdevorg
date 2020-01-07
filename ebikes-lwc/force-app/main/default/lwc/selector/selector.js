d67750e3f402bda4d58610a7c4db03d6288f8ea0
Sawyer Kennell <38391716+sawyersz@users.noreply.github.com>
2020-01-07 14:10:03 -0700

    starting from scratch

--- ebikes-lwc/force-app/main/default/lwc/selector/selector.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track, api } from 'lwc';
-
-export default class Selector extends LightningElement {
-    @track selectedProductId;
-
-    handleProductSelected(evt) {
-        this.selectedProductId = evt.detail;
-    }
-}
