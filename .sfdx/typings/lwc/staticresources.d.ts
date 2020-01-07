84011155d58755d62a284b9c51048380d36a65f5
Sawyer Kennell <38391716+sawyersz@users.noreply.github.com>
2020-01-07 13:17:25 -0700

    project cleanup

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/bike_assets" {
+    var bike_assets: string;
+    export default bike_assets;
+}
+declare module "@salesforce/resourceUrl/lccEbikes" {
+    var lccEbikes: string;
+    export default lccEbikes;
+}
