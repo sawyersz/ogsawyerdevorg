4e3c347d5dccf48020f80fa18c48d424573f83c6
Sawyer Kennell <38391716+sawyersz@users.noreply.github.com>
2020-01-08 08:14:45 -0700

    building component out, subtabs

--- FSLComponent/force-app/main/default/lwc/fSLcomponent/fSLcomponent.js
+++ FSLComponent/force-app/main/default/lwc/fSLcomponent/fSLcomponent.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track} from 'lwc';
+import fetchDataHelper from './fetchDataHelper';
 
-export default class FSLcomponent extends LightningElement {}
+const columns = [
+    { label: 'Label', fieldName: 'name' },
+    { label: 'Website', fieldName: 'website', type: 'url' },
+    { label: 'Phone', fieldName: 'phone', type: 'phone' },
+    { label: 'Balance', fieldName: 'amount', type: 'currency' },
+    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+];
+
+export default class FSLcomponent extends LightningElement {
+    @track data = [];
+    @track columns = columns;
+
+    async connectedCallback() {
+        const data = await fetchDataHelper({ amountOfRecords: 100 });
+        this.data = data;
+    }
+}
