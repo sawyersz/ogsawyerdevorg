d750890c5605d3dd791ecb03eabdbf0e998e33d0
Sawyer Kennell <38391716+sawyersz@users.noreply.github.com>
2020-01-14 08:58:45 -0700

    building component out, cleaning up

--- FSLComponent/force-app/main/default/lwc/fSLcomponent/fetchDataHelper.js
+++ FSLComponent/force-app/main/default/lwc/fSLcomponent/fetchDataHelper.js
@@ -8,14 +8,5 @@ const recordMetadata = {
 };
 
 export default function fetchDataHelper({ amountOfRecords }) {
-    return fetch('https://data-faker.herokuapp.com/collection', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({
-            amountOfRecords,
-            recordMetadata,
-        }),
-    }).then(response => response.json());
+    return null
 }
